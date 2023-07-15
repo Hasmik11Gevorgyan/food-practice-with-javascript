d248657bae8e4cc7dc1fa35ea13bf26ecc548d41
unknown <you@example.com>
2023-07-15 04:28:06 -0700

    something new is added

--- js/script.js
+++ js/script.js
@@ -1,40 +1,39 @@
 "use - strict"
 
 window.addEventListener("DOMContentLoaded", function () {
-    const tabsHeaders = document.querySelectorAll(".tabheader__item");
-    const tabsContents = document.querySelectorAll(".tabcontent")
-    const tabsHeadersParent = document.querySelector(".tabheader__items ");
+    // tab logic start
+	const tabsHeaders = document.querySelectorAll(".tabheader__item");
+	const tabsContents = document.querySelectorAll(".tabcontent");
+	const tabsHeadersParent = document.querySelector(".tabheader__items");
 
+	function hideTabContent() {
+		tabsContents.forEach(tabContent => {
+			tabContent.classList.add("hide");
+			tabContent.classList.remove("show", "fade");
+		});
+		tabsHeaders.forEach(tabHeader => tabHeader.classList.remove("tabheader__item_active"));
+	}
 
-    function hideTabContent() {
-        tabsContents.forEach(item => {
-            item.classList.add("hide");
-            item.classList.remove("show", "fade")
-        });
-        tabsHeaders.forEach(item => {
-            item.classList.remove("tabheader__item_active")
-        })
-    }
+	function showTabContent(i = 1) {
+		tabsContents[i].classList.add("show", "fade");
+		tabsContents[i].classList.remove("hide");
+		tabsHeaders[i].classList.add("tabheader__item_active");
+	}
 
-    function showTabContent(i = 0) {
-        tabsContents[i].classList.add("show", "fade");
-        tabsContents[i].classList.remove("hide");
-        tabsHeaders[i].classList.add("tabheader__item_active")
-    }
+	hideTabContent();
+	showTabContent();
 
-
-    tabsHeadersParent.addEventListener("click", (e) => {
-        if (e.target && e.target.matches(".tabheader__item ")) {
-            tabsHeaders.forEach((tabheader, index) => {
-                if (e.target === tabheader) {
-                    hideTabContent();
-                    showTabContent(index);
-                }
-            })
-        }
-        hideTabContent();
-        showTabContent(index);
-    })
+	tabsHeadersParent.addEventListener("click", (e) => {
+		if (e.target && e.target.matches(".tabheader__item")) {
+			tabsHeaders.forEach((tabHeader, index) => {
+				if (e.target === tabHeader) {
+					hideTabContent();
+					showTabContent(index);
+				}
+			});
+		}
+	});
+	// tab logic end
 
 
     //  timer logicn start
